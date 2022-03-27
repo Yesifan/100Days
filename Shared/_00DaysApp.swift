730257f2ede8ad484b8c516547c7b0874e98c723
YeSifan <504936436@qq.com>
2022-03-27 10:30:52 +0800

    Initial Commit

---
+++ Shared/_00DaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  _00DaysApp.swift
+//  Shared
+//
+//  Created by 叶思凡 on 2022/3/27.
+//
+
+import SwiftUI
+
+@main
+struct _00DaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
